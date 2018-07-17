099d714b751d495cafd44c6b4850f17fed5ee935
aldiyusuf <aldianto880@gmail.com>
2018-07-17 19:31:13 +0800

    test

---
+++ index.php
@@ -0,0 +1,165 @@
+<?php
+include"config.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>WEDDING ORGANIZER</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-default navbar-fixed-top" style="background:#d74b35;">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          
+          <a class="navbar-brand" href="#"><img src="images/sk.png" style="width:100px; margin-top: -7px;"></a>
+        </div>
+        <div class="collapse navbar-collapse">
+
+
+        <div class="nav navbar-nav navbar-right">
+         <ul id="nav">
+          <li ><a href="index.php" style="color:#fff;"><span class="glyphicon glyphicon-home"> Home | </span></a></li>
+          <li><a href="" style="color:#fff;" ><span class="glyphicon glyphicon-list"> Kategori | </span></a>
+<ul>
+<li><?php include("kat.php");?></li>
+
+</ul>
+</li>
+          <li class="a"><a href="cara_pesan.php" style="color:#fff;"><span class="glyphicon glyphicon-question-sign"> Cara Belanja | </span></a></li>
+          <li class="a"><a href="login.php" style="color:#fff;"><span class="glyphicon glyphicon-log-in"> Login</span></a></li>
+          </ul>
+          <div class="clear"></div>
+          
+          </div>
+      </div>
+    </nav>
+    <div class="jumbotron">
+      <div class="row">
+      <div class="col-md-4" style="margin:30px;">
+     <img src="img/bc.jpg">   
+    </div>
+      <div class="col-md-6" style="margin-left:70px; margin-top: 80px;">
+        <h2><b><center>Persembahan spesial untuk anda, calon mempelai..</center><h1 style="color:#f97b61;"></b></h1></h2>
+        <p><br>Terimakasih anda telah mengunjungi website RIAS PENGANTIN DARTIK Ini adalah website official kami yang selanjutnya akan memberikan berbagai macam informasi terkait dengan kegiatan yang biasanya kami lakukan info lebih lengkap hubungi 0813-9319-7442.</br>
+		<br>Creative Wedding Organizer merupakan Wedding Organizer, Wedding Planner yang menyediakan Jasa maupun Paket Wedding untuk persiapan sampai pelaksanaan acara pernikahan dari berbagai kebutuhan yang di perlukan untuk sebuah Pesta pernikahan dengan budjet minimal hasil yang megah dan elegant.</br>
+		</p>
+      </div>
+    </div>
+    </div>
+<div style="margin-top: -30px; width:100%,height:50px;text-align:center;background:#d74b35;color:#fff;line-height:60px;font-size:20px;">
+<b>Pelaminan</b>
+</div>
+    <div class="container">
+      <div class="row">
+      <?php
+      @$idkat = $_GET['id'] ;
+      $qrybukukat = mysql_query("SELECT * from buku where id_ketegori='$idkat'");
+      $qrybuku= mysql_query("SELECT * from buku");
+      if($idkat==0){
+      while($buku = mysql_fetch_array($qrybuku)) {
+      ?>
+      
+        <div class="col-md-3" style="margin-top:20px;">
+        <div class="buku">
+        <center><img src="gambar/<?php echo $buku['gambar'] ?>" style="margin-top:20px; width:210px;height:190px;"></center>
+         <h3 style="text-align:center; color:#f97b61;"><?php echo $buku['judul'] ?></h3>
+          <center><b>Harga</b> Rp.<?php echo $buku['harga']; ?></center> 
+          <center><b>Stok</b> (<?php echo $buku['stok']; ?>)</center>
+          <center><a class="btn btn-danger" href="detail.php?id=<?php echo $buku['id_buku'] ?>" role="button" style="margin-top:10px;">View details &raquo;</a></center>
+         </div>
+        </div>
+        <?php } }
+        else{ while($buku1 = mysql_fetch_array($qrybukukat)){?>
+            <div class="col-md-3" style="margin-top:20px;">
+        <div class="buku">
+        <center><img src="gambar/<?php echo $buku1['gambar'] ?>" style="margin-top:20px;width:210px;height:190px;"></center>
+        <h3 style="text-align:center; color:#f97b61; "><?php echo $buku1['judul'] ?></h3>
+          <center><b>Harga</b> Rp.<?php echo $buku1['harga']; ?></center> 
+          <center><b>Stok</b> (<?php echo $buku1['stok']; ?>)</center>
+          <center><a class="btn btn-danger" href="detail.php?id=<?php echo $buku1['id_buku'] ?>" role="button" style="margin-top:10px;">View details &raquo;</a></center>
+         </div>
+        </div>
+          <?php }} ?>
+      </div>
+
+      <hr>
+
+      
+    </div> 
+     
+    <div class="footer" style="width:100%;height:270px;color:#fff;background:#d74b35;">
+      <div class="row" style="background:#7e7c78;">
+      <div class="col-md-4">
+      <div style="margin:50px;height:120px;">
+        <center>
+        <ul>
+          <li style="color:#f97b61"><h3><b>Tentang weddingORGANIZER</b></h3></li>
+        </ul></center>
+          <hr>
+        <ul>
+          <li><b>weddingORGANIZER</b> adalah</li>
+          <li>Sebuah jasa khusus yang dan</li>
+          <li>membantu calon pengantin</li>
+          <li>keluarga dalam pelaksaan</li>
+          <li>acara pernikahan.</li>
+        </ul>
+      </div>
+      </div>
+      <div class="col-md-4">
+      <div style="margin:50px;height:120px;">
+        <center>
+        <ul>
+          <li style="color:#f97b61"><h3><b>Alamat Kami</b></h3></li>
+        </ul></center>
+          <hr>
+    
+          <ul>
+          <li>Jl.Jakenan-winong,km 30</li>
+          <li>jakenan,juwana</li>
+          <li>Pati</li>
+          <li>Jawa Tengah, Indonesia</li>
+          <li></li>
+        </ul>
+      
+      </div>
+      </div>
+      <div class="col-md-4">
+      <div style="margin:50px;height:120px;">
+        <center>
+        <ul>
+          <ul>
+          <li style="color:#f97b61"><h3><b>Contact Us</b></h3></li>
+          <hr>
+         <div class="row">
+          <div class="col-md-4">
+          <a href="www.fecebook.com"><img src="images/fb.png" style="width:70px;height:75px;  "></a>
+          </div>
+          <div class="col-md-4">
+          <a href="www.googleplus.com"><img src="images/gp.png" style="width:70px;height:75px;"></a>
+          </div>
+          <div class="col-md-4">
+          <a href=""><img src="images/Twitter.png" style="width:70px;height:75px;"></a>
+          </div>
+         </div>
+        </ul>
+        </center>
+      </div>
+      </div>
+      </div>
+        <div class="copyright" style="line-height:50px;">
+        <center>&copy; @2018 web development </center>
+        </div>
+      </div>
+  </body>
+</html>
---
+++ page/admin/home.php
@@ -0,0 +1,19 @@
+<div class="jumbotron" style="margin-top:50px">
+      <div class="row">
+      <div class="col-md-5">
+     <img src="../../img/cd.jpg" style="width:400px;height:400px;">   
+    </div>
+      <div class="col-md-6" style="margin-top: 50px;">
+        <h2><b></b></h2>
+        <p>Paket Spesial Pengantin antara lain :</p>
+		<br>1.  Siraman s.d Resepsi meliputi :</br>
+		<br>- Make up siraman</br>
+<br>- Make up midodareni</br>
+<br>- Make up ijab</br>
+<br>- Make up resepsi (Muslim/Solo Putri/Jogja Putri)</br>
+<br>-  Solo basahan + 1.000.000</br>
+
+<br>-  Paes Ageng + 2.000.000</br>
+      </div>
+    </div>
+    </div>
